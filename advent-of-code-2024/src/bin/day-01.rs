81b0a4b8361b48baf65659cdbe36810832d84d36
playbahn <plabanroy69@gmail.com>
2024-12-02 23:03:45 +0530

    2024-day2

--- advent-of-code-2024/src/bin/day-01.rs
+++ advent-of-code-2024/src/bin/day-01.rs
@@ -2,45 +2,46 @@ use std::collections::HashMap;
 
 fn main() {
     // common
-    let (mut list1, mut list2): (Vec<u32>, Vec<u32>) = std::fs::read_to_string("input/day-01.txt")
-        .unwrap()
-        .lines()
-        .map(|line| line.split_whitespace().collect::<Vec<&str>>())
-        .fold(
-            (Vec::with_capacity(1000), Vec::with_capacity(1000)),
-            |(mut list1, mut list2), pair_vec| {
-                list1.push(pair_vec[0].parse().unwrap());
-                list2.push(pair_vec[1].parse().unwrap());
-                (list1, list2)
-            },
-        );
+    let (mut left_list, mut right_list): (Vec<u32>, Vec<u32>) =
+        std::fs::read_to_string("input/day-01.txt")
+            .unwrap()
+            .lines()
+            .map(|line| line.split_whitespace().collect::<Vec<&str>>())
+            .fold(
+                (Vec::with_capacity(1000), Vec::with_capacity(1000)),
+                |(mut list1, mut list2), pair_vec| {
+                    list1.push(pair_vec[0].parse().unwrap());
+                    list2.push(pair_vec[1].parse().unwrap());
+                    (list1, list2)
+                },
+            );
 
-    list1.sort();
-    list2.sort();
+    left_list.sort();
+    right_list.sort();
 
     // part 1
     let mut dist = 0;
 
-    for (num1, num2) in list1.iter().zip(list2.iter()) {
-        dist += num1.abs_diff(*num2);
+    for (left_id, right_id) in left_list.iter().zip(right_list.iter()) {
+        dist += left_id.abs_diff(*right_id);
     }
 
     println!("dist: {dist}");
 
     // part 2
-    let mut right_num_freq: HashMap<u32, usize> = HashMap::with_capacity(1000);
+    let mut right_id_freq: HashMap<u32, usize> = HashMap::with_capacity(1000);
     let mut similarity_score = 0;
 
-    // all right nums and how many times they appear
-    for right_num in list2 {
-        right_num_freq
-            .entry(right_num)
+    // all right ids and how many times they appear
+    for right_id in right_list {
+        right_id_freq
+            .entry(right_id)
             .and_modify(|freq| *freq += 1)
             .or_insert(1);
     }
 
-    for left_num in list1 {
-        similarity_score += left_num as usize * right_num_freq.get(&left_num).unwrap_or(&0);
+    for left_id in left_list {
+        similarity_score += right_id_freq.get(&left_id).unwrap_or(&0) * left_id as usize;
     }
 
     println!("similarity_score: {similarity_score}");
--- advent-of-code-2024/src/bin/day-02.rs
+++ advent-of-code-2024/src/bin/day-02.rs
@@ -1,1 +1,58 @@
-fn main() {}
+use std::cmp::Ordering;
+
+fn is_safe(report: &[u8]) -> bool {
+    report
+        .windows(2)
+        .fold(
+            (true, report[0].cmp(&report[1])),
+            |(safe, prev_ord), adj| -> (bool, Ordering) {
+                (
+                    safe && prev_ord == adj[0].cmp(&adj[1])
+                        && (1..4).contains(&adj[0].abs_diff(adj[1])),
+                    prev_ord,
+                )
+            },
+        )
+        .0
+}
+
+fn main() {
+    let reports: Vec<Vec<u8>> = std::fs::read_to_string("input/day-02.txt")
+        .unwrap()
+        .lines()
+        .map(|report| -> Vec<u8> {
+            report
+                .split_whitespace()
+                .map(|level| level.parse().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let (mut safe1, mut safe2) = (0u32, 0u32);
+
+    'report: for report in reports {
+        // part 1
+        if is_safe(&report) {
+            safe1 += 1;
+            safe2 += 1;
+            continue;
+        }
+
+        // part 2
+        for skip_idx in 0..report.len() {
+            if is_safe(
+                &report
+                    .iter()
+                    .enumerate()
+                    .filter(|(idx, _)| *idx != skip_idx)
+                    .map(|(_, level)| *level)
+                    .collect::<Vec<u8>>(),
+            ) {
+                safe2 += 1;
+                continue 'report;
+            }
+        }
+    }
+
+    println!("safe1: {safe1} safe2: {safe2}");
+}
