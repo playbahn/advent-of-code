2b135df9b33c17d674166812b86a3be8fb934cc3
playbahn <plabanroy69@gmail.com>
2024-12-10 15:25:50 +0530

    2024-10

--- advent-of-code-2024/src/bin/day-08.rs
+++ advent-of-code-2024/src/bin/day-08.rs
@@ -5,19 +5,38 @@ enum Part {
     Two,
 }
 
-type Coords = (isize, isize); // `(.0, .1)` = (x, y)
+type Coords = (isize, isize); // (x, y)
 
 fn within_bounds(antinode: Coords) -> bool {
     -1 < antinode.0 && -1 < antinode.1 && antinode.0 < 50 && antinode.1 < 50
 }
 
-fn find_antinodes(node1: Coords, node2: Coords, part: Part) -> HashSet<Coords> {
-    let mut antinodes = HashSet::new();
-    let dist = (node2.0 - node1.0, node2.1 - node1.1);
-    let mut next_antinode = (node2.0 + dist.0, node2.1 + dist.1);
+fn find_antinodes_aux(node: Coords, dist: Coords, part: &Part) -> Vec<Coords> {
+    let mut antinodes = if let Part::One = part {
+        vec![]
+    } else {
+        // from https://adventofcode.com/2024/day/8 (part 2):
+        //
+        // > an antinode occurs at any point that is perfectly in line
+        // > with two antennas of the same frequency
+        //
+        // at any *POINT* - slipped from mind
+        //
+        // > In fact, the three T-frequency antennas are all exactly
+        // > in line with two antennas, so they are all also antinodes!
+        //
+        // So even if there is no space left on the line crossing `node1`
+        // and `node2` in either direction `node1 -> node2` or  `node2 ->
+        // node1` that is within map bounds, `node1` is in line with
+        // `node1` ITSELF and `node2`, and the same goes for `node2`.
+        // BRAH. That was REALLY F***IN HARD to understand.
+        vec![node]
+    };
+
+    let mut next_antinode = (node.0 + dist.0, node.1 + dist.1);
     loop {
         if within_bounds(next_antinode) {
-            antinodes.insert(next_antinode);
+            antinodes.push(next_antinode);
         } else {
             break;
         }
@@ -26,22 +45,15 @@ fn find_antinodes(node1: Coords, node2: Coords, part: Part) -> HashSet<Coords> {
         }
         next_antinode = (next_antinode.0 + dist.0, next_antinode.1 + dist.1);
     }
+    antinodes
+}
 
-    if let Part::Two = part {
-        // from https://adventofcode.com/2024/day/8 (part 2):
-        // 
-        // > In fact, the three T-frequency antennas are all exactly
-        // > in line with two antennas, so they are all also antinodes!
-        // 
-        // So even if there is no space left on the line crossing `node1`
-        // and `node2` is either direction `node1 -> node2` or  `node2 ->
-        // node1` that is within map bounds, `node1` is in line with
-        // `node1` ITSELF and `node2`, and the same goes for `node2`.
-        // BRAH. That was REALLY F***IN HARD to understand.
-        antinodes.insert(node1);
-        antinodes.insert(node2);
-    }
-    
+fn find_antinodes(node1: Coords, node2: Coords, part: Part) -> Vec<Coords> {
+    let dist = (node2.0 - node1.0, node2.1 - node1.1);
+    // direction: node1 -> node2; distance: positive
+    let mut antinodes = find_antinodes_aux(node2, (dist.0, dist.1), &part);
+    // direction: node2 -> node1; distance: negative
+    antinodes.append(&mut find_antinodes_aux(node1, (-dist.0, -dist.1), &part));
     antinodes
 }
 
@@ -57,8 +69,8 @@ fn main() {
                 // casted to `isize` to steer clear of underflow issues in
                 // `find_antinodes`. 50 non-`\n` chars + 1 `\n` every line
                 let node = (
-                    offset as isize % 51,
-                    offset as isize / 51,
+                    offset as isize % 51, // x-coordinate
+                    offset as isize / 51, // y-coordinate
                 );
                 map.entry(freq)
                     .and_modify(|nodes| nodes.push(node))
@@ -68,9 +80,9 @@ fn main() {
 
     // part 1
     for nodes in map.values() {
-        for (idx1, node1) in nodes.iter().enumerate() {
-            for (_, node2) in nodes.iter().enumerate().filter(|(idx2, _)| *idx2 != idx1) {
-                antinodes.extend(find_antinodes(*node1, *node2, Part::One));
+        for node1 in 0..nodes.len() - 1 {
+            for node2 in node1 + 1..nodes.len() {
+                antinodes.extend(find_antinodes(nodes[node1], nodes[node2], Part::One));
             }
         }
     }
@@ -79,9 +91,9 @@ fn main() {
 
     // part 2
     for nodes in map.values() {
-        for (idx1, node1) in nodes.iter().enumerate() {
-            for (_, node2) in nodes.iter().enumerate().filter(|(idx2, _)| *idx2 != idx1) {
-                antinodes.extend(find_antinodes(*node1, *node2, Part::Two));
+        for node1 in 0..nodes.len() - 1 {
+            for node2 in node1 + 1..nodes.len() {
+                antinodes.extend(find_antinodes(nodes[node1], nodes[node2], Part::Two));
             }
         }
     }
--- advent-of-code-2024/src/bin/day-10.rs
+++ advent-of-code-2024/src/bin/day-10.rs
@@ -1,1 +1,63 @@
-fn main() {}
+use std::collections::HashSet;
+
+const PATH: &str = "input/day-10.txt";
+
+type Coords = (usize, usize);
+
+fn get_peaks(curr: Coords, map: &[[u8; 59]; 59], peaks: &mut HashSet<Coords>, rating: &mut u16) {
+    if map[curr.0][curr.1] == 9 {
+        peaks.insert(curr);
+        *rating += 1;
+        return;
+    }
+
+    if let Some(x) = curr.0.checked_sub(1) {
+        if map[x][curr.1] == 1 + map[curr.0][curr.1] {
+            get_peaks((x, curr.1), map, peaks, rating);
+        }
+    }
+
+    if 1 + curr.0 < 59 && map[1 + curr.0][curr.1] == 1 + map[curr.0][curr.1] {
+        get_peaks((1 + curr.0, curr.1), map, peaks, rating);
+    }
+
+    if let Some(y) = curr.1.checked_sub(1) {
+        if map[curr.0][y] == 1 + map[curr.0][curr.1] {
+            get_peaks((curr.0, y), map, peaks, rating);
+        }
+    }
+
+    if 1 + curr.1 < 59 && map[curr.0][1 + curr.1] == 1 + map[curr.0][curr.1] {
+        get_peaks((curr.0, 1 + curr.1), map, peaks, rating);
+    }
+}
+
+fn main() {
+    let mut map: [[u8; 59]; 59] = [[255; 59]; 59];
+    let mut heads: Vec<Coords> = Vec::new();
+
+    std::fs::read_to_string(PATH)
+        .unwrap()
+        .lines()
+        .enumerate()
+        .for_each(|(y, line)| {
+            line.chars().enumerate().for_each(|(x, ch)| {
+                map[x][y] = ch.to_digit(10).unwrap() as u8;
+                if map[x][y] == 0 {
+                    heads.push((x, y))
+                }
+            })
+        });
+
+    let mut part1 = 0usize;
+    let mut part2 = 0u16;
+    let mut peaks: HashSet<Coords> = HashSet::new();
+
+    for head in heads {
+        get_peaks(head, &map, &mut peaks, &mut part2);
+        part1 += peaks.len();
+        peaks.clear();
+    }
+
+    println!("part1: {part1} part2: {part2}")
+}
