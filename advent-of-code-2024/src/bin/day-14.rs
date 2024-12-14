a4a562553630f5d1e49fa69ea6757d43d54c49d6
playbahn <plabanroy69@gmail.com>
2024-12-15 00:56:38 +0530

    2024-14-Two

--- advent-of-code-2024/src/bin/day-14.rs
+++ advent-of-code-2024/src/bin/day-14.rs
@@ -1,68 +1,52 @@
+/// NOT TO SELF - DO NOT EVER CREATE/USE/IMPLEMENT CRATE TYPES
+/// (e.g. STRUCTS) FOR PROBLEM SOLVING EVER AGAIN
+use std::collections::HashSet;
+
 const INPUT: &str = "input/day-14.txt";
 
 const UBX: u8 = 101;
 const UBY: u8 = 103;
-const TIME: u8 = 100;
+const TIME: u16 = 100;
+const LCM_UBX_UBY: u16 = UBX as u16 * UBY as u16;
 
-/// Position
-struct P {
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Position {
     x: u8,
     y: u8,
 }
 
-/// Velocity
-type V = P;
-/// Movement
-type M = P;
+type Displacement = Position;
 
-impl std::ops::Add<M> for P {
-    type Output = P;
+impl std::ops::Mul<u16> for &Displacement {
+    type Output = Displacement;
 
-    fn add(self, rhs: M) -> Self::Output {
-        P {
+    fn mul(self, rhs: u16) -> Self::Output {
+        Displacement {
+            x: ((self.x as u16 * rhs) % UBX as u16) as u8,
+            y: ((self.y as u16 * rhs) % UBY as u16) as u8,
+        }
+    }
+}
+
+impl std::ops::Add<Displacement> for &Position {
+    type Output = Position;
+
+    fn add(self, rhs: Displacement) -> Self::Output {
+        Position {
             x: (self.x + rhs.x) % UBX,
             y: (self.y + rhs.y) % UBY,
         }
     }
 }
 
-/// Part 2 straight up mocked this :_)
-impl std::ops::Mul<u8> for V {
-    type Output = M;
-
-    fn mul(self, rhs: u8) -> Self::Output {
-        M {
-            x: ((self.x as u16 * rhs as u16) % UBX as u16) as u8,
-            y: ((self.y as u16 * rhs as u16) % UBY as u16) as u8,
-        }
+impl std::ops::AddAssign<&Displacement> for Position {
+    fn add_assign(&mut self, rhs: &Displacement) {
+        self.x = (self.x + rhs.x) % UBX;
+        self.y = (self.y + rhs.y) % UBY;
     }
 }
 
-fn pvs(input: &str) -> impl Iterator<Item = (P, V)> + '_ {
-    input
-        .lines()
-        .map(|robot| robot.split_once(' ').unwrap())
-        .map(|(p, v)| {
-            (
-                p[2..].split_once(',').unwrap(),
-                v[2..].split_once(',').unwrap(),
-            )
-        })
-        .map(|(p, v)| {
-            (
-                P {
-                    x: p.0.parse().unwrap(),
-                    y: p.1.parse().unwrap(),
-                },
-                V {
-                    x: (v.0.parse::<i16>().unwrap() + UBX as i16) as u8 % UBX,
-                    y: (v.1.parse::<i16>().unwrap() + UBY as i16) as u8 % UBY,
-                },
-            )
-        })
-}
-
-fn get_quadrant(p: P) -> Option<usize> {
+fn get_quadrant(p: Position) -> Option<usize> {
     use std::cmp::Ordering::*;
 
     match (p.x.cmp(&(UBX >> 1)), p.y.cmp(&(UBY >> 1))) {
@@ -75,16 +59,73 @@ fn get_quadrant(p: P) -> Option<usize> {
 }
 
 fn main() {
-    let mut quadrants: [u16; 4] = [0, 0, 0, 0];
+    let (positions, displacements): (Vec<Position>, Vec<Displacement>) =
+        std::fs::read_to_string(INPUT)
+            .unwrap()
+            .lines()
+            .map(|robot| robot.split_once(' ').unwrap())
+            .map(|(p, v)| {
+                (
+                    p[2..].split_once(',').unwrap(),
+                    v[2..].split_once(',').unwrap(),
+                )
+            })
+            .map(|(p, v)| {
+                (
+                    Position {
+                        x: p.0.parse().unwrap(),
+                        y: p.1.parse().unwrap(),
+                    },
+                    Displacement {
+                        x: (v.0.parse::<i16>().unwrap() + UBX as i16) as u8 % UBX,
+                        y: (v.1.parse::<i16>().unwrap() + UBY as i16) as u8 % UBY,
+                    },
+                )
+            })
+            .collect();
 
-    for (p, v) in pvs(&std::fs::read_to_string(INPUT).unwrap()) {
-        if let Some(quadrant) = get_quadrant(p + v * TIME) {
+    let mut quadrants: [u16; 4] = [0, 0, 0, 0];
+    for (position, displacement) in positions.iter().zip(displacements.iter()) {
+        if let Some(quadrant) = get_quadrant(position + displacement * TIME) {
             quadrants[quadrant] += 1;
         }
     }
 
     let safety_factor: u32 = quadrants.iter().map(|num| *num as u32).product();
     println!("Part 1: {}", safety_factor);
-    
-    
+
+    let mut positions: Vec<Position> = positions;
+    let mut distincts: HashSet<Position> = HashSet::with_capacity(500);
+    let mut set_pixels: Vec<Position> = Vec::with_capacity(500);
+    let mut img = bmp::Image::new(UBX as u32, UBY as u32);
+
+    println!("Writing bitmaps to img/ ...");
+    // Every x-coord will be at its original position after UBX time-steps, and y-coord
+    // after UBY. Both of the x- and y-corods will be at their original position
+    // simultaneously at LCM(UBX, UBY) = `LCM_UBX_UBY` time-steps. After which, the
+    // same coords will start repeating.
+    for time in 1..LCM_UBX_UBY {
+        for (position, displacement) in positions.iter_mut().zip(displacements.iter()) {
+            *position += displacement;
+        }
+
+        distincts.extend(positions.clone());
+        for y in 0..UBY {
+            for x in 0..UBX {
+                if distincts.contains(&Position { x, y }) {
+                    img.set_pixel(x as u32, y as u32, bmp::consts::RED);
+                    set_pixels.push(Position { x, y });
+                }
+            }
+        }
+        
+        img.save(format!("img/{}.bmp", time)).unwrap();
+        for set_pixel in &mut set_pixels {
+            img.set_pixel(set_pixel.x as u32, set_pixel.y as u32, bmp::consts::BLACK);
+        }
+        
+        distincts.clear();
+        set_pixels.clear();
+    }
+    println!();
 }
