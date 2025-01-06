d2159e3fb2e3657a628f2cc59a1ac78bb669618d
playbahn <plabanroy69@gmail.com>
2025-01-06 19:46:01 +0530

    2024-9-Two

--- aoc-2024/src/bin/day-09.rs
+++ aoc-2024/src/bin/day-09.rs
@@ -2,15 +2,37 @@ use std::collections::VecDeque;
 
 const INPUT: &str = "input/day-09.txt";
 
+/// Lookup Table for constant time checksumming of each file.
+/// https://cprimozic.net/blog/optimizing-advent-of-code-2024/#constant-time-checksumming
+#[allow(clippy::identity_op)]
+const LUT: [usize; 10] = [
+    0,
+    0,
+    0 + 1,
+    0 + 1 + 2,
+    0 + 1 + 2 + 3,
+    0 + 1 + 2 + 3 + 4,
+    0 + 1 + 2 + 3 + 4 + 5,
+    0 + 1 + 2 + 3 + 4 + 5 + 6,
+    0 + 1 + 2 + 3 + 4 + 5 + 6 + 7,
+    0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8,
+];
+
 /// Any file.
 #[derive(Debug)]
 struct File {
     /// file ID
     id: usize,
     /// Blocks left of this file ID that can be moved to the front
-    blocks: u8,
+    blocks: usize,
     /// Block offset on `Disk` (for Part 2)
     offset: usize,
+}
+
+impl File {
+    fn new(id: usize, blocks: usize, offset: usize) -> Self {
+        Self { id, blocks, offset }
+    }
 }
 
 /// Reader on actual disk map.
@@ -20,188 +42,136 @@ struct Reader {
     pos: usize,
     /// No. of blocks the file is consuming (if `pos` is even),
     /// or the no. of empty blocks (if `pos` is odd).
-    blocks: u8,
+    blocks: usize,
 }
 
-#[derive(Clone)]
-struct Block(Option<u16>);
-
-struct Disk(Vec<Block>);
-
-impl Disk {
-    fn swap_blocks(&mut self, to: usize, from: usize, count: u8) {
-        (0..count.into()).for_each(|offset| self.swap(to + offset, from + offset));
+impl Reader {
+    fn new(pos: usize, blocks: usize) -> Self {
+        Self { pos, blocks }
     }
 }
 
 fn main() {
-    let p1start = std::time::Instant::now();
+    let t1 = std::time::Instant::now();
 
     let disk_map = std::fs::read_to_string(INPUT).unwrap();
     let disk_map = disk_map.trim_end();
 
-    let mut disk_reader = disk_map.char_indices().map(|ch| Reader {
-        pos: ch.0,
-        blocks: ch.1.to_digit(10).unwrap() as u8,
-    });
+    let mut disk_reader = disk_map
+        .char_indices()
+        .map(|(pos, blocks)| Reader::new(pos, blocks.to_digit(10).unwrap() as usize));
 
-    let last_id = disk_map.len() >> 1;
-    let mut files = (disk_map.chars().rev().step_by(2))
-        .enumerate()
-        .map(|(file_id, blocks)| File {
-            id: last_id - file_id,
-            blocks: blocks.to_digit(10).unwrap() as u8,
-            // not reqd for Part 1
-            offset: 0,
-        });
+    let mut files = disk_map
+        .char_indices()
+        .rev()
+        .step_by(2)
+        .map(|(id, blocks)| File::new(id >> 1, blocks.to_digit(10).unwrap() as usize, 0));
 
     // Position of where the writer would be after any operation.
-    let mut writer_pos: usize = 0;
-    let mut file = files.next().unwrap();
-    let mut reader = disk_reader.next().unwrap();
-    let mut part1: usize = 0;
+    let mut fragger: usize = 0;
+    // dummy vars
+    let mut file = File::new(usize::MAX, 0, usize::MAX);
+    let mut read = Reader::new(usize::MAX, 0);
+    let mut p1: usize = 0;
 
     loop {
-        if reader.blocks == 0 {
-            if let Some(next) = disk_reader.next() {
-                reader = next
-            } else {
-                break;
-            };
+        if read.blocks == 0 {
+            match disk_reader.next() {
+                Some(next) => read = next,
+                None => break,
+            }
         }
         if file.blocks == 0 {
-            if let Some(next) = files.next() {
-                file = next
-            } else {
-                break;
-            };
+            match files.next() {
+                Some(next) => file = next,
+                None => break,
+            }
         }
 
-        println!("{reader:?}");
-        // wrong: causes errors when `reader.pos` is odd
+        // wrong (causes errors when `reader.pos` is odd) ---
         // `reader.pos >> 1 > file.id`
-        // right:
-        if reader.pos > file.id << 1 {
+        // right ---
+        if read.pos > file.id << 1 {
             break;
         }
 
-        if reader.pos & 0b1 == 0 {
-            let to_read = if reader.pos >> 1 == file.id {
+        if read.pos & 0b1 == 0 {
+            let read_blocks = if read.pos >> 1 == file.id {
                 file.blocks
             } else {
-                reader.blocks
-            }
-            as usize;
+                read.blocks
+            };
 
-            part1 += (writer_pos..writer_pos + to_read).sum::<usize>() * (reader.pos >> 1);
-            writer_pos += to_read;
-            reader.blocks = 0;
+            p1 += (read_blocks * fragger + LUT[read_blocks]) * (read.pos >> 1);
+            fragger += read_blocks;
+            read.blocks = 0;
         } else {
-            match reader.blocks.cmp(&file.blocks) {
+            match read.blocks.cmp(&file.blocks) {
                 std::cmp::Ordering::Less => {
-                    part1 +=
-                        (writer_pos..writer_pos + reader.blocks as usize).sum::<usize>() * file.id;
-                    writer_pos += reader.blocks as usize;
-                    file.blocks -= reader.blocks;
-                    reader.blocks = 0;
+                    p1 += (read.blocks * fragger + LUT[read.blocks]) * file.id;
+                    fragger += read.blocks;
+                    file.blocks -= read.blocks;
+                    read.blocks = 0;
                 }
                 std::cmp::Ordering::Equal => {
-                    part1 += (writer_pos..writer_pos + reader.blocks as usize).sum::<usize>() * file.id;
-                    writer_pos += reader.blocks as usize;
-                    (file.blocks, reader.blocks) = (0, 0);
+                    p1 += (read.blocks * fragger + LUT[read.blocks]) * file.id;
+                    fragger += read.blocks;
+                    (file.blocks, read.blocks) = (0, 0);
                 }
                 std::cmp::Ordering::Greater => {
-                    part1 += (writer_pos..writer_pos + file.blocks as usize).sum::<usize>() * file.id;
-                    writer_pos += file.blocks as usize;
-                    reader.blocks -= file.blocks;
+                    p1 += (file.blocks * fragger + LUT[file.blocks]) * file.id;
+                    fragger += file.blocks;
+                    read.blocks -= file.blocks;
                     file.blocks = 0;
                 }
             }
         }
     }
 
-    println!("{}s", p1start.elapsed().as_secs_f64());
-    println!("part1: {part1}");
+    println!("p1: {p1} t1: {:?}", t1.elapsed());
 
-    let p2start = std::time::Instant::now();
+    let t2 = std::time::Instant::now();
 
-    // K = free spaces length/span; V = starting positions (offsets) of free spaces.
+    // K = free spaces span; V = offsets of free spaces.
     let mut free_spaces: [VecDeque<usize>; 10] = [const { VecDeque::new() }; 10];
-    let mut disk_files: [VecDeque<usize>; 10] = [const { VecDeque::new() }; 10];
     let mut files: Vec<File> = Vec::with_capacity(10000);
     let mut offset = 0usize;
-    let mut disk: Disk = Disk(Vec::with_capacity(100000));
 
     disk_map.char_indices().for_each(|(index, blocks)| {
         let blocks = blocks.to_digit(10).unwrap() as usize;
 
         if index & 0b1 == 0 {
-            disk_files[blocks].push_back(offset);
-            let id = index >> 1;
-            disk.extend(std::iter::repeat(Block(Some((index >> 1) as u16))).take(blocks));
-            files.push(File {
-                id: index >> 1,
-                blocks: blocks as u8,
-                offset,
-            });
-        } else if blocks != 0 {
+            files.push(File::new(index >> 1, blocks, offset));
+        } else if blocks > 0 {
             free_spaces[blocks].push_back(offset);
-            disk.extend(std::iter::repeat(Block(None)).take(blocks));
         }
 
         offset += blocks;
     });
 
-    // while let Some(x) = files.pop() {
-    //     println!("{x:?}");
-    // }
+    let mut p2: usize = 0;
 
-    // println!("disk_files: {disk_files:?}");
-    // println!("free_spaces: {free_spaces:?}");
-    // println!("{disk:?}");
+    while let Some(file) = files.pop() {
+        let mut new_offset = file.offset;
+        let mut index = file.blocks;
 
-    println!("{}s", p2start.elapsed().as_secs_f64());
-}
+        (file.blocks..10).for_each(|blocks| {
+            if let Some(offset) = free_spaces[blocks].front() {
+                if *offset < new_offset {
+                    new_offset = *offset;
+                    index = blocks;
+                }
+            }
+        });
 
-impl std::ops::Deref for Block {
-    type Target = Option<u16>;
+        p2 += (file.blocks * new_offset + LUT[file.blocks]) * file.id;
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl std::ops::DerefMut for Block {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl std::fmt::Debug for Block {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.0 {
-            Some(id) => write!(f, "{id}"),
-            None => write!(f, "_"),
+        let old_offset = free_spaces[index].pop_front();
+        if index > file.blocks {
+            free_spaces[index - file.blocks].push_front(old_offset.unwrap() + file.blocks);
+            free_spaces[index - file.blocks].make_contiguous().sort();
         }
     }
-}
 
-impl std::ops::Deref for Disk {
-    type Target = Vec<Block>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl std::ops::DerefMut for Disk {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl std::fmt::Debug for Disk {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.0)
-    }
+    println!("p2: {p2} t2: {:?}", t2.elapsed());
 }
